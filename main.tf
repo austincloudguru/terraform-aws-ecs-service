886c558be95162708d50bfb58b6619bf7bf3b31c
Mark Honomichl <mark@austincloud.guru>
2019-11-08 10:58:41 -0600

    Trying something new

--- main.tf
+++ main.tf
@@ -5,25 +5,46 @@ data "aws_ecs_cluster" "this" {
   cluster_name = var.ecs_cluster_name
 }
 
-
+//
 data "template_file" "task_definition" {
   template = <<EOF
-  [
-    {
-      "name": "$${service_name}",
-      "image": "$${image_name}",
-      "essential": true
-    }
-  ]
-
-  EOF
-  vars = {
-    service_name = var.service_name
-    image_name = var.image_name
-//    port_mappings = jsonencode(var.port_mappings)
-//    mount_points = jsonencode(var.mount_points)
-//    env_variables = jsonencode(var.env_variables)
+[
+  {
+    "name": "jenkins-master",
+    "image": "jenkins/jenkins",
+    "cpu": 128,
+    "memory": 1024,
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 8080,
+        "hostPort": 8080
+      },
+      {
+        "containerPort": 50000,
+        "hostPort": 50000
+      }
+    ],
+    "mountPoints": [
+      {
+        "sourceVolume": "jenkins_home",
+        "containerPath": "/var/jenkins_home"
+      }
+    ]
   }
+]
+//EOF
+//  vars = {
+//    service_name = var.service_name
+//    image_name = var.image_name
+////    port_mappings = jsonencode(var.port_mappings)
+////    mount_points = jsonencode(var.mount_points)
+////    env_variables = jsonencode(var.env_variables)
+//  }
+}
+
+data "template_file" "task_template" {
+  template = file("templates/service.json.tmpl")
 }
 
 data "aws_caller_identity" "current" {}
