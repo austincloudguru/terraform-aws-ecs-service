440e28329010f7c241aff2962c82d6003cedaf15
Mark Honomichl <mark@austincloud.guru>
2019-11-08 12:47:14 -0600

    Added the load balancer

--- main.tf
+++ main.tf
@@ -44,6 +44,11 @@ resource "aws_ecs_service" "main" {
   cluster = data.aws_ecs_cluster.this.id
   desired_count = var.service_desired_count
   iam_role = aws_iam_role.instance_role.arn
+  load_balancer {
+    target_group_arn = aws_lb_target_group.https_target_group.arn
+    container_name = var.service_name
+    container_port = lookup(var.port_mappings[0], "hostPort")
+  }
 }
 
 #------------------------------------------------------------------------------
