3a7cb0afefccb204356ac477f683d2130d66f5ee
Mark Honomichl <mark@austincloud.guru>
2019-11-08 11:13:23 -0600

    Changing the way

--- main.tf
+++ main.tf
@@ -6,31 +6,31 @@ data "aws_ecs_cluster" "this" {
 }
 
 //
-data "template_file" "task_definition" {
-  template = <<EOF
-[
-  {
-    "name": "$${service_name}",
-    "image": "$${image_name}",
-    "essential": true,
-    "portMappings": [$${port_mappings}],
-    "mountPoints": [
-      {
-        "sourceVolume": "jenkins_home",
-        "containerPath": "/var/jenkins_home"
-      }
-    ]
-  }
-]
-EOF
-  vars = {
-    service_name = var.service_name
-    image_name = var.image_name
-    port_mappings = jsonencode(var.port_mappings)
-//    mount_points = jsonencode(var.mount_points)
-//    env_variables = jsonencode(var.env_variables)
-  }
-}
+//data "template_file" "task_definition" {
+//  template = <<EOF
+//[
+//  {
+//    "name": "$${service_name}",
+//    "image": "$${image_name}",
+//    "essential": true,
+//    "portMappings": [$${port_mappings}],
+//    "mountPoints": [
+//      {
+//        "sourceVolume": "jenkins_home",
+//        "containerPath": "/var/jenkins_home"
+//      }
+//    ]
+//  }
+//]
+//EOF
+//  vars = {
+//    service_name = var.service_name
+//    image_name = var.image_name
+//    port_mappings = jsonencode(var.port_mappings)
+////    mount_points = jsonencode(var.mount_points)
+////    env_variables = jsonencode(var.env_variables)
+//  }
+//}
 
 //data "template_file" "task_template" {
 //  template = file("templates/service.json.tmpl")
@@ -42,9 +42,20 @@ data "aws_caller_identity" "current" {}
 # Launch Docker Service
 #------------------------------------------------------------------------------
 resource "aws_ecs_task_definition" "this" {
-  container_definitions = data.template_file.task_definition.rendered
   family = var.service_name
   execution_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
+  container_definitions = jsonencode([
+    {
+      cpu          = 10
+      essential    = true
+      image        = var.image_name
+      memory       = 128
+      name         = var.service_name
+      portMappings = var.port_mappings
+      mountPoints  = var.mount_points
+      environment  = var.env_variables
+    }
+  ])
 }
 
 resource "aws_ecs_service" "main" {
