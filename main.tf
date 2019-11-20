0703c48e06328a12446c6ee8f3d0546f0c083aad
Mark Honomichl <mark@austincloud.guru>
2019-11-20 09:15:21 -0600

    Removing the load balancer and cleaning some things up.

--- main.tf
+++ main.tf
@@ -1,110 +1,4 @@
 #------------------------------------------------------------------------------
-# Collect necessary data
-#------------------------------------------------------------------------------
-data "aws_ecs_cluster" "this" {
-  cluster_name = var.ecs_cluster_name
-}
-
-data "aws_caller_identity" "current" {}
-
-data "aws_lb" "this" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  name = var.lb_name
-}
-
-data "aws_lb_listener" "this" {
-  count = length(var.lb_name) > 0 && var.create_listener ? 0 : 1
-  load_balancer_arn = data.aws_lb.this[0].arn
-  port              = 443
-}
-
-data "aws_vpc" "this" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc_name]
-  }
-}
-
-data "aws_route53_zone" "external" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  name = "${var.tld}."
-}
-
-#------------------------------------------------------------------------------
-# Launch Docker Service
-#------------------------------------------------------------------------------
-resource "aws_ecs_task_definition" "this" {
-  family             = var.service_name
-  execution_role_arn = aws_iam_role.ecs_exec_role.arn
-  network_mode = var.network_mode
-  container_definitions = jsonencode([
-    {
-      name         = var.service_name
-      image        = var.image_name
-      cpu          = var.service_cpu
-      memory       = var.service_memory
-      essential    = var.essential
-      privileged   = var.privileged
-      command      = var.command
-      portMappings = var.port_mappings
-      mountPoints  = var.mount_points
-      environment  = var.env_variables
-      linuxParameters = var.linux_parameters
-    }
-  ])
-  dynamic "volume" {
-    for_each = var.volumes
-    content {
-      name      = volume.value.name
-      host_path = lookup(volume.value, "host_path", null)
-
-      dynamic "docker_volume_configuration" {
-        for_each = lookup(volume.value, "docker_volume_configuration", [])
-        content {
-          autoprovision = lookup(docker_volume_configuration.value, "autoprovision", null)
-          driver        = lookup(docker_volume_configuration.value, "driver", null)
-          driver_opts   = lookup(docker_volume_configuration.value, "driver_opts", null)
-          labels        = lookup(docker_volume_configuration.value, "labels", null)
-          scope         = lookup(docker_volume_configuration.value, "scope", null)
-        }
-      }
-    }
-  }
-  tags = merge(
-    {
-      "Name" = "${var.service_name}-td"
-    },
-    var.tags
-  )
-}
-
-resource "aws_ecs_service" "main" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  depends_on      = [aws_lb_target_group.https_target_group]
-  name            = var.service_name
-  task_definition = aws_ecs_task_definition.this.arn
-  cluster         = data.aws_ecs_cluster.this.id
-  desired_count   = var.service_desired_count
-  iam_role        = aws_iam_role.instance_role.arn
-  load_balancer {
-    target_group_arn = aws_lb_target_group.https_target_group[0].arn
-    container_name   = var.service_name
-    container_port   = lookup(var.port_mappings[0], "containerPort")
-  }
-}
-
-resource "aws_ecs_service" "main-no-lb" {
-  count = length(var.lb_name) > 0 ? 0 : 1
-  depends_on      = [aws_lb_target_group.https_target_group]
-  name            = var.service_name
-  task_definition = aws_ecs_task_definition.this.arn
-  cluster         = data.aws_ecs_cluster.this.id
-  desired_count   = var.service_desired_count
-  iam_role        = aws_iam_role.instance_role.arn
-}
-
-#------------------------------------------------------------------------------
 # Create the executor role
 #------------------------------------------------------------------------------
 resource "aws_iam_role" "ecs_exec_role" {
@@ -162,7 +56,7 @@ resource "aws_iam_role" "instance_role" {
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
   tags = merge(
     {
-      "Name" = "${var.ecs_cluster_name}-task"
+      "Name" = "${var.service_name}-task"
     },
     var.tags
   )
@@ -213,117 +107,74 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
 }
 
 #------------------------------------------------------------------------------
-# Create an ACM Certificate using DNS Validation
-# Variables:
-#     tld           Top Level Domain
-#     app_name      Application Name for the Certificate
+# Launch Docker Service
 #------------------------------------------------------------------------------
-resource "aws_acm_certificate" "acm_cert" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  domain_name       = "${var.service_name}.${var.tld}"
-  validation_method = "DNS"
-}
+resource "aws_ecs_task_definition" "this" {
+  family             = var.service_name
+  execution_role_arn = aws_iam_role.ecs_exec_role.arn
+  network_mode = var.network_mode
+  container_definitions = jsonencode([
+    {
+      name              = var.service_name
+      image             = var.image_name
+      cpu               = var.service_cpu
+      memory            = var.service_memory
+      essential         = var.essential
+      privileged        = var.privileged
+      command           = var.command
+      portMappings      = var.port_mappings
+      mountPoints       = var.mount_points
+      environment       = var.environment
+      linuxParameters   = var.linux_parameters
+      logConfiguration  = var.log_configuration
+    }
+  ])
+  dynamic "volume" {
+    for_each = var.volumes
+    content {
+      name      = volume.value.name
+      host_path = lookup(volume.value, "host_path", null)
 
-resource "aws_route53_record" "cert_validation_record" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  name    = aws_acm_certificate.acm_cert[0].domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.acm_cert[0].domain_validation_options.0.resource_record_type
-  zone_id = data.aws_route53_zone.external[0].zone_id
-  records = [aws_acm_certificate.acm_cert[0].domain_validation_options.0.resource_record_value]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "default" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  certificate_arn         = aws_acm_certificate.acm_cert[0].arn
-  validation_record_fqdns = [aws_route53_record.cert_validation_record[0].fqdn]
-}
-
-#------------------------------------------------------------------------------
-# Create an HTTPS LB
-# Requires aws_acm_certificate - DNS Validation
-# Variables:
-#     app_name      Application Name for the Certificate
-#     app_port      Port the Application LB Listens on
-#------------------------------------------------------------------------------
-resource "aws_lb_listener" "https_alb_listener" {
-  count = length(var.lb_name) > 0 && var.create_listener ? 1 : 0
-  load_balancer_arn = data.aws_lb.this[0].arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.acm_cert[0].arn
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.https_target_group[0].arn
-  }
-}
-
-resource "aws_lb_target_group" "https_target_group" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  name     = "${var.service_name}-ecs-tg"
-  port     = lookup(var.port_mappings[0], "hostPort")
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.this[0].id
-
-  health_check {
-    interval          = 60
-    path              = var.health_check_path
-    timeout           = 5
-    healthy_threshold = 2
-    port              = lookup(var.port_mappings[0], "hostPort")
+      dynamic "docker_volume_configuration" {
+        for_each = lookup(volume.value, "docker_volume_configuration", [])
+        content {
+          autoprovision = lookup(docker_volume_configuration.value, "autoprovision", null)
+          driver        = lookup(docker_volume_configuration.value, "driver", null)
+          driver_opts   = lookup(docker_volume_configuration.value, "driver_opts", null)
+          labels        = lookup(docker_volume_configuration.value, "labels", null)
+          scope         = lookup(docker_volume_configuration.value, "scope", null)
+        }
+      }
+    }
   }
   tags = merge(
     {
-      "Name" = "${var.service_name}-ecs-tg"
+      "Name" = "${var.service_name}-td"
     },
     var.tags
   )
 }
 
-resource "aws_lb_listener_rule" "https_alb_listener_rule" {
-  count = length(var.lb_name) > 0 && var.create_listener ? 1 : 0
-  listener_arn = aws_lb_listener.https_alb_listener[0].arn
-  priority     = 1
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.https_target_group[0].arn
-  }
-  condition {
-    field = "host-header"
-    values = [
-    aws_route53_record.alb_dns[0].fqdn]
-  }
-}
+//resource "aws_ecs_service" "main" {
+//  count = length(var.lb_name) > 0 ? 1 : 0
+//  depends_on      = [aws_lb_target_group.https_target_group]
+//  name            = var.service_name
+//  task_definition = aws_ecs_task_definition.this.arn
+//  cluster         = var.ecs_cluster_id
+//  desired_count   = var.service_desired_count
+//  iam_role        = aws_iam_role.instance_role.arn
+//  load_balancer {
+//    target_group_arn = aws_lb_target_group.https_target_group[0].arn
+//    container_name   = var.service_name
+//    container_port   = lookup(var.port_mappings[0], "containerPort")
+//  }
+//}
 
-resource "aws_lb_listener_rule" "https_alb_listener_rule_no_listener" {
-  count = length(var.lb_name) > 0 && var.create_listener ? 0 : 1
-  listener_arn = data.aws_lb_listener.this[0].arn
-  priority     = 1
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.https_target_group[0].arn
-  }
-  condition {
-    field = "host-header"
-    values = [
-    aws_route53_record.alb_dns[0].fqdn]
-  }
-}
-
-#------------------------------------------------------------------------------
-# Create DNS Record
-# Variables:
-#     app_name      Application Name for the Certificate
-#------------------------------------------------------------------------------
-resource "aws_route53_record" "alb_dns" {
-  count = length(var.lb_name) > 0 ? 1 : 0
-  name    = var.service_name
-  type    = "A"
-  zone_id = data.aws_route53_zone.external[0].zone_id
-  alias {
-    evaluate_target_health = false
-    name                   = data.aws_lb.this[0].dns_name
-    zone_id                = data.aws_lb.this[0].zone_id
-  }
+resource "aws_ecs_service" "main-no-lb" {
+  count = length(var.lb_name) > 0 ? 0 : 1
+  name            = var.service_name
+  task_definition = aws_ecs_task_definition.this.arn
+  cluster         = var.ecs_cluster_id
+  desired_count   = var.service_desired_count
+  iam_role        = aws_iam_role.instance_role.arn
 }
--- variable.tf
+++ variable.tf
@@ -1,5 +1,13 @@
-variable "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
+#------------------------------------------------------------------------------
+# Variables for ECS Service Module
+#------------------------------------------------------------------------------
+variable "service_name" {
+  description = "Name of the service being deployed"
+  type        = string
+}
+
+variable "ecs_cluster_id" {
+  description = "ID of the ECS cluster"
   type        = string
 }
 
@@ -15,50 +23,10 @@ variable "lb_name" {
   default     = ""
 }
 
-variable "vpc_name" {
-  description = "Name of the VPC to work in"
-  type        = string
-  default     = ""
-}
-
-variable "tld" {
-  description = "Top Level Domain to use"
-  type        = string
-  default     = ""
-}
-
-variable "health_check_path" {
-  description = "Health Check Path"
-  type        = string
-  default     = "/"
-}
-
-variable "volumes" {
-  description = "Task volume definitions as list of configuration objects"
-  type = list(object({
-    host_path = string
-    name      = string
-    docker_volume_configuration = list(object({
-      autoprovision = bool
-      driver        = string
-      driver_opts   = map(string)
-      labels        = map(string)
-      scope         = string
-    }))
-  }))
-  default = []
-}
-
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
   default     = {}
-}
-
-variable "create_listener" {
-  description = "Create the https listener"
-  type        = bool
-  default     = false
 }
 
 variable "task_iam_policies" {
@@ -69,19 +37,6 @@ variable "task_iam_policies" {
     resources = list(string)
   }))
   default = []
-}
-
-variable "network_mode" {
-  description = "The Network Mode to run the container at"
-  type = string
-  default = "bridge"
-}
-#------------------------------------------------------------------------------
-# Container Definition Variables
-#------------------------------------------------------------------------------
-variable "service_name" {
-  description = "Name of the service being deployed"
-  type        = string
 }
 
 variable "image_name" {
@@ -126,8 +81,8 @@ variable "port_mappings" {
     protocol      = string
   }))
   default = [{
-    hostPort      = 12345
-    containerPort = 12345
+    hostPort      = 80
+    containerPort = 80
     protocol      = "tcp"
   }]
 }
@@ -138,14 +93,70 @@ variable "mount_points" {
   default     = []
 }
 
-variable "env_variables" {
+variable "environment" {
   description = "Environmental Variables to pass to the container"
-  type        = list(object({}))
-  default     = []
+  type        = list(object({
+    name  = string
+    value = string
+  }))
+  default     = null
 }
 
 variable "linux_parameters" {
   description = "Additional Linux Parameters"
-  type = object({})
-  default = {}
+  type = object({
+    capabilities = object({
+      add = list(string)
+      drop = list(string)
+    })
+  })
+  default = null
+}
+
+variable "network_mode" {
+  description = "The Network Mode to run the container at"
+  type = string
+  default = "bridge"
+}
+
+variable "volumes" {
+  description = "Task volume definitions as list of configuration objects"
+  type = list(object({
+    host_path = string
+    name      = string
+    docker_volume_configuration = list(object({
+      autoprovision = bool
+      driver        = string
+      driver_opts   = map(string)
+      labels        = map(string)
+      scope         = string
+    }))
+  }))
+  default = []
+}
+
+variable "tld" {
+  description = "Top Level Domain to use"
+  type        = string
+  default     = ""
+}
+
+variable "external_zone_id" {
+  description = "The External Zone ID to use"
+  type        = string
+  default     = ""
+}
+
+variable "log_configuration" {
+  description = "Log configuration options to send to a custom log driver for the container."
+
+  type = object({
+    logDriver = string
+    options   = map(string)
+    secretOptions = list(object({
+      name      = string
+      valueFrom = string
+    }))
+  })
+  default     = null
 }
